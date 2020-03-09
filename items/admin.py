ede2e4758ee367a9f30bcb149f6a2b170b4bf042
Aru <aru.yelemes23@gmail.com>
2020-03-09 15:02:40 +0600

    register Company to admin.py

--- items/admin.py
+++ items/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import JobArea,Hunter
+from .models import JobArea,Hunter,Company
 # Register your models here.
 admin.site.register(JobArea)
 admin.site.register(Hunter)
+admin.site.register(Company)
